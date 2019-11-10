e169655cee9c36e9fa6e9564e25c849613c2a15f
kiranscaria <kiranscaria@outlook.com>
2019-11-10 16:03:54 +0530

    Cleaned-up the PDFReader fragment.

--- app/src/main/java/com/example/aspmobile/fragments/PDFReader.kt
+++ app/src/main/java/com/example/aspmobile/fragments/PDFReader.kt
@@ -16,30 +16,18 @@ import android.view.ViewGroup
 
 import com.example.aspmobile.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Activities that contain this fragment must implement the
- * [PDFReader.OnFragmentInteractionListener] interface
- * to handle interaction events.
- * Use the [PDFReader.newInstance] factory method to
- * create an instance of this fragment.
- */
+private const val pdfSource = "param1"
+
+
 class PDFReader : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var pdfSource: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            pdfSource = it.getString(com.example.aspmobile.fragments.pdfSource)
         }
     }
 
@@ -70,38 +58,17 @@ class PDFReader : Fragment() {
         listener = null
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson [Communicating with Other Fragments]
-     * (http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
     interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         fun onFragmentInteraction(uri: Uri)
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PDFReader.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(fileSource: String) =
             PDFReader().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(com.example.aspmobile.fragments.pdfSource, fileSource)
                 }
             }
     }
